526e534986118314db9519bf09b1af5bd3aaa308
tarek <tareksaier@gmail.com>
2014-12-08 17:28:14 +0100

    rebuilt lists OOP style

--- anihilist.py
+++ anihilist.py
@@ -19,12 +19,175 @@ CLIENT_ID   = 'sirtetris-eky4q'
 CLIENT_SEC  = 'AcF5qNj4St50Bf0mPLLU9BgpRObb'
 USER        = None
 REDIR_FOO   = 'http%3A%2F%2Fmoc.sirtetris.com%2Fanihilist%2Fechocode.php'
-DISP_KEY    = None
-SORT_KEY    = None
-ID_MODE     = False
-RAW_MODE    = False
 LIST_ANIME  = 0
 LIST_XDCC   = 1
+UP          = -1
+DOWN        = 1
+
+class TheScreen:
+    def set(src):
+        TheScreen.src = src
+    def get():
+        return TheScreen.src
+
+class Anime:
+    def __init__(self, al_data, xdcc_info, parent):
+        self.al_id = str(al_data['anime']['id'])
+        self.parent = parent
+        self.title = {}
+        self.title['ja'] = al_data['anime']['title_japanese'].strip()
+        self.title['en'] = al_data['anime']['title_english'].strip()
+        self.title['ro'] = al_data['anime']['title_romaji'].strip()
+        ep_total = al_data['anime']['total_episodes']
+        if ep_total > 0:
+            self.ep_total = str(ep_total)
+        else:
+            self.ep_total = '?'
+        self.ep_seen = str(al_data['episodes_watched'])
+        self.pkg_list = None
+        if self.al_id in xdcc_info:
+            pkg_list_raw = xdcc_info[self.al_id]
+            pkg_list_arg = []
+            for pkg in pkg_list_raw:
+                pkg.seen = (int(pkg.ep_num) <= int(self.ep_seen))
+                pkg_list_arg.append(pkg)
+            self.pkg_list = PackageList(pkg_list_arg, self)
+            self._set_xdcc_cue(pkg_list_raw)
+        else:
+            self.xdcc_cue = ''
+    def _set_xdcc_cue(self, pkg_list_raw):
+        ep_nums = [int(pkg.ep_num) for pkg in pkg_list_raw]
+        if len(ep_nums) > 0:
+            ep_newest = max(ep_nums)
+        else:
+            ep_newest = 0
+        ep_seen = int(self.ep_seen)
+        if ep_newest > ep_seen:
+            self.xdcc_cue = '*' + str(ep_newest-ep_seen)
+        elif ep_newest > 0:
+            self.xdcc_cue = '*'
+        else:
+            self.xdcc_cue = '\''
+
+class Package():
+    def __init__(self, ep_num, pkg_num, group, line, seen):
+        self.ep_num = ep_num
+        self.pkg_num = pkg_num
+        self.group = group
+        self.line = line
+        self.seen = seen
+
+class List:
+    def __init__(self, lisd):
+        self.scr = TheScreen.get()
+        self.lisd = lisd
+        self.cursor = 0
+        self.offset = 0
+        (y_max,x_max) = self.scr.getmaxyx()
+        self.end_screen = y_max-2
+        self.x_max = x_max
+        self.end_list = len(lisd)-1
+    def _getOnScreen(self):
+        start_idx = self.offset
+        end_idx = min(self.end_screen, self.end_list) + self.offset
+        return self.lisd[start_idx:(end_idx+1)]
+    def scroll(self, delta):
+        goal_screen = self.cursor + delta
+        goal_list = self.cursor + self.offset + delta
+        if goal_list>self.end_list:
+            return
+        elif goal_screen<0:
+            if self.offset==0:
+                return
+            else:
+                self.offset += delta
+        elif goal_screen>self.end_screen:
+            self.offset += delta
+        else:
+            self.cursor += delta
+
+class AnimeList(List):
+    def __init__(self, anilist_data, list_key, xdcc_info):
+        self.anilist_data = anilist_data
+        self.list_key = list_key
+        self.id_mode = False
+        self._setListLanguage()
+        list_raw = anilist_data['lists'][list_key]
+        list_processed = []
+        for al_data in list_raw:
+            list_processed.append(Anime(al_data, xdcc_info, self))
+        sortd = sorted(list_processed, key=lambda k: k.title[self.sort_key])
+        List.__init__(self, sortd)
+    def toggleIDMode(self):
+        self.id_mode = not self.id_mode
+    def getUnderCursor(self):
+        return self.lisd[self.cursor + self.offset]
+    def display(self):
+        sub_list = self._getOnScreen()
+        y = 0
+        for anime in sub_list:
+            if self.cursor == y: self.scr.standout()
+            self._addListLine(y, anime)
+            if self.cursor == y: self.scr.standend()
+            y+=1
+    def _addListLine(self, y, anime):
+        title = self._getTitle(anime)
+        ep_info = ' [{0}/{1}]'.format(anime.ep_seen, anime.ep_total)
+        self.scr.addstr(y, 0, ' '*self.x_max)
+        self.scr.addstr(y, 0, title)
+        self.scr.addstr(y, (self.x_max-len(ep_info)), ep_info)
+    def _getTitle(self, anime):
+        if self.id_mode:
+            return anime.al_id
+        else:
+            return anime.title[self.title_key] + ' ' + anime.xdcc_cue
+    def _setListLanguage(self):
+        if self.anilist_data['title_language'] == 'japanese':
+            self.title_key = 'ja'
+            self.sort_key = 'ro'
+        elif self.anilist_data['title_language'] == 'romaji':
+            self.title_key = 'ro'
+            self.sort_key = 'ro'
+        else:
+            self.title_key = 'en'
+            self.sort_key = 'en'
+
+class PackageList(List):
+    def __init__(self, pkgs, parent):
+        self.parent = parent
+        self.raw_mode = False
+        sortd = sorted(pkgs, key=lambda k: k.ep_num)
+        List.__init__(self, sortd)
+    def toggleRawMode(self):
+        self.raw_mode = not self.raw_mode
+    def yankUnderCursor(self):
+        pkg = self.lisd[self.cursor + self.offset]
+        try:
+            os.system('echo -n "{0}" | xclip'.format(pkg.pkg_num))
+        except:
+            pass
+    def display(self):
+        sub_list = self._getOnScreen()
+        y = 0
+        for pkg in sub_list:
+            idx = y + self.offset
+            anime = self.parent
+            anime_list = anime.parent
+            if self.raw_mode:
+                line = pkg.line
+            else:
+                title = anime.title[anime_list.title_key]
+                line = '{0} -> [{1}] {2} {3}'.format(pkg.pkg_num, pkg.group,
+                                title, pkg.ep_num)
+            if int(pkg.ep_num) > int(anime.ep_seen):
+                self.scr.standout()
+            self.scr.addstr(y, 0, ' '*self.x_max)
+            if self.cursor == y:
+                self.scr.addstr(y, 0, '- ' + line)
+            else:
+                self.scr.addstr(y, 0, '  ' + line)
+            y+=1
+        self.scr.standend()
 
 def setUser():
     global USER
@@ -110,108 +273,11 @@ def updateWatchedCount(anime, delta):
     old = int(anime['episodes_watched'])
     new = old+delta
     a_id = int(anime['anime']['id'])
+    #FIXME: accesstoken needs to be header
+    #(see: https://github.com/Josh-Star/AniList-API-Docs/issues/7)
     url = ('/api/animelist?access_token={0}').format(getAccessToken())
     data = urllib.parse.urlencode({'id':a_id, 'episodes_watched':new})
     return callAPI('PUT', url, data=data)
-
-def cursesShutdown():
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-
-def addListLine(scr, y, x_max, anime, xdcc_info):
-    title = getTitle(anime, xdcc_info)
-    ep_total = anime['anime']['total_episodes']
-    if ep_total == 0: ep_total = '?'
-    ep_seen = anime['episodes_watched']
-    ep_info = ' [{0}/{1}]'.format(ep_seen,ep_total)
-    scr.addstr(y, 0, ' '*x_max)
-    scr.addstr(y, 0, title)
-    scr.addstr(y, (x_max-len(ep_info)), ep_info)
-
-def getTitle(anime, xdcc_info):
-    a_id = str(anime['anime']['id'])
-    if ID_MODE:
-        return a_id
-    else:
-        title = anime['anime'][DISP_KEY].strip()
-        if a_id in xdcc_info:
-            return XDCCTitle(anime, title, xdcc_info[a_id])
-        else:
-            return title
-
-def XDCCTitle(anime, title, pkgs):
-    ep_nums = [int(pkg['ep_num']) for pkg in pkgs]
-    if len(ep_nums) > 0:
-        ep_newest = max(ep_nums)
-    else:
-        ep_newest = 0
-    ep_seen = int(anime['episodes_watched'])
-    if ep_newest > ep_seen:
-        return title + ' *' + str(ep_newest-ep_seen)
-    elif ep_newest > 0:
-        return title + ' *'
-    else:
-        return title + ' \''
-
-def printList(scr, anime_watchings, selected, offset, xdcc_info):
-    (y_max,x_max)=scr.getmaxyx()
-    y=0
-    while y+1<y_max and y+offset<len(anime_watchings):
-        anime = anime_watchings[y+offset]
-        if selected == y:
-            scr.standout()
-            addListLine(scr, y, x_max, anime, xdcc_info)
-            scr.standend()
-        else:
-            addListLine(scr, y, x_max, anime, xdcc_info)
-        y+=1
-
-def printPkgList(scr, anime, selected, xdcc_info, offset):
-    (y_max,x_max)=scr.getmaxyx()
-    ep_seen = int(anime['episodes_watched'])
-    pkgs = xdcc_info[str(anime['anime']['id'])]
-    pkgss = sorted(pkgs, key=lambda k: k['ep_num'])
-    y=0
-    while y+1<y_max and y+offset<len(pkgss):
-        pkg = pkgss[y+offset]
-        if RAW_MODE:
-            line = pkg['line']
-        else:
-            line = '{0} -> [{1}] {2} {3}'.format(pkg['pkg_num'], pkg['group'],
-                            anime['anime'][DISP_KEY].strip(), pkg['ep_num'])
-        if int(pkg['ep_num']) > ep_seen:
-            scr.standout()
-        scr.addstr(y, 0, ' '*x_max)
-        if selected == y:
-            scr.addstr(y, 0, '- ' + line)
-        else:
-            scr.addstr(y, 0, '  ' + line)
-        y+=1
-    scr.standend()
-
-def setListLanguage(anime_list_data):
-    global DISP_KEY
-    global SORT_KEY
-
-    if anime_list_data['title_language'] == 'japanese':
-        DISP_KEY = 'title_japanese'
-        SORT_KEY = 'title_romaji'
-    elif anime_list_data['title_language'] == 'romaji':
-        DISP_KEY = 'title_romaji'
-        SORT_KEY = 'title_romaji'
-    else:
-        DISP_KEY = 'title_english'
-        SORT_KEY = 'title_english'
-
-def toggleIDs():
-    global ID_MODE
-    ID_MODE = not ID_MODE
-
-def toggleRAW():
-    global RAW_MODE
-    RAW_MODE = not RAW_MODE
 
 def getXDCCInfo():
     # xdcc.json file
@@ -240,104 +306,60 @@ def getXDCCInfo():
         matches = re.findall(patt, xdcc_text)
         pkgs = []
         for m in matches:
-            pkg = {}
-            pkg['line'] = m[0]
-            pkg['pkg_num'] = m[1]
-            pkg['ep_num'] = m[2]
-            pkg['group'] = entry['group']
-            pkgs.append(pkg)
+            pkgs.append(Package(m[2], m[1], entry['group'], m[0], None))
         xdcc_info[key] = pkgs
     return xdcc_info
 
-def updateAnimeWatchings(anime_list_data):
-    setListLanguage(anime_list_data)
-    anime_lists = anime_list_data['lists']
-    anime_watching = anime_lists['watching']
-    anime_watchings = sorted(anime_watching,
-                             key=lambda k: k['anime'][SORT_KEY])
-    return anime_watchings
-
-def XDCCNavLimit(anime, xdcc_info):
-    key = str(anime['anime']['id'])
-    if key in xdcc_info:
-        pkgs = xdcc_info[key]
-        return len(pkgs)
-    else:
-        return 0
-
-def yankXDCC(xdcc_info, xdcc_key, idx):
-    pkgs = xdcc_info[xdcc_key]
-    pkg_num = pkgs[idx]['pkg_num']
-    try:
-        os.system('echo -n "{0}" | xclip'.format(pkg_num))
-    except:
-        pass
+def getUpdatedAnimeList():
+    anilist_data = getAnimeList()
+    xdcc_info = getXDCCInfo()
+    return AnimeList(anilist_data, 'watching', xdcc_info)
 
 def main(stdscr):
-    anime_watchings = updateAnimeWatchings(getAnimeList())
-    xdcc_info = getXDCCInfo()
+    TheScreen.set(stdscr)
+    anime_list = getUpdatedAnimeList()
 
     curses.use_default_colors()
     curses.curs_set(0)
     stdscr.clear()
-    (y_max,x_max)=stdscr.getmaxyx()
-
-    y_max_nav=[None]*2
-    y_max_nav[LIST_ANIME] = min((len(anime_watchings)-1), y_max-2)
-    list_max_nav=[None]*2
-    list_max_nav[LIST_ANIME] = len(anime_watchings)-1
-    curs_y=[None]*2
-    curs_y[LIST_ANIME]=0
-    curs_y[LIST_XDCC]=0
-    offset=[None]*2
-    offset[LIST_ANIME]=0
-    offset[LIST_XDCC]=0
 
     c=None
     list_type=0
 
     while c != 'q':
         stdscr.move(0,0)
-        anime_curs = anime_watchings[curs_y[LIST_ANIME]+offset[LIST_ANIME]]
-        xdcc_key = str(anime_curs['anime']['id'])
+        anime_curs = anime_list.getUnderCursor()
 
         if list_type==LIST_ANIME:
-            printList(stdscr, anime_watchings, curs_y[LIST_ANIME],
-                            offset[LIST_ANIME], xdcc_info)
+            anime_list.display()
         elif list_type==LIST_XDCC:
-            printPkgList(stdscr, anime_curs, curs_y[LIST_XDCC], xdcc_info,
-                            offset[LIST_XDCC])
+            anime_curs.pkg_list.display()
 
         c = stdscr.getkey()
         if c==NAV_U:
-            if curs_y[list_type]==0 and offset[list_type] != 0:
-                offset[list_type]-=1
-            elif curs_y[list_type]>0:
-                curs_y[list_type]-=1
+            if list_type == LIST_ANIME:
+                anime_list.scroll(UP)
+            if list_type == LIST_XDCC:
+                anime_curs.pkg_list.scroll(UP)
         if c==NAV_D:
-            if curs_y[list_type]<y_max_nav[list_type]:
-                curs_y[list_type]+=1
-            elif curs_y[list_type]+offset[list_type]<list_max_nav[list_type]:
-                offset[list_type]+=1
+            if list_type == LIST_ANIME:
+                anime_list.scroll(DOWN)
+            if list_type == LIST_XDCC:
+                anime_curs.pkg_list.scroll(DOWN)
         if c==NAV_L:
             pass
         if c==NAV_R:
-            updateWatchedCount(anime_curs, 1)
-            anime_watchings = updateAnimeWatchings(getAnimeList())
+            #updateWatchedCount(anime_curs, 1)
+            anime_list = getUpdatedAnimeList()
         if c=='i' and list_type==LIST_ANIME:
-            toggleIDs()
+            anime_list.toggleIDMode()
         if c=='c' and list_type==LIST_XDCC:
-            toggleRAW()
+            anime_curs.pkg_list.toggleRawMode()
         if c=='y' and list_type==LIST_XDCC:
-            yankXDCC(xdcc_info, xdcc_key, curs_y[LIST_XDCC])
-        if c=='s' and xdcc_key in xdcc_info:
+            anime_curs.pkg_list.yankUnderCursor()
+        if c=='s' and not anime_curs.pkg_list is None:
             stdscr.clear()
             list_type = 1-list_type
-            curs_y[LIST_XDCC]=0
-            offset[LIST_XDCC]=0
-            pkg_count = XDCCNavLimit(anime_curs, xdcc_info)
-            list_max_nav[LIST_XDCC] = pkg_count-1
-            y_max_nav[LIST_XDCC] = min(pkg_count-1, y_max-2)
 
 if __name__ == '__main__':
     setup()
